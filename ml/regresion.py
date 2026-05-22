fd609ab19fba5ce3b6dc12f6a6401446db16ad4f
armanghazi <ghaziaskari@gmail.com>
2026-05-22 14:19:14 +0200

    Corregir error tipográfico en el comentario de la función evaluar_modelo

--- ml/regresion.py
+++ ml/regresion.py
@@ -7,7 +7,7 @@ def evaluar_modelo(modelo, X_test, y_test, magnitud=""):
     Devuelve métricas sin generar gráficos.
     """
 
-    # 1) Prediccione
+    # 1) Predicciones
     y_pred = modelo.predict(X_test)
 
     # 2) Métricas principales
