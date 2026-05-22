32dfef72111408c51962f3358d77f4c5cfc9e17e
armanghazi <ghaziaskari@gmail.com>
2026-05-22 14:03:08 +0200

    Actualizar versión a 0.2.0 en setup.py, corregir dependencias y mejorar descripción del paquete

--- setup.py
+++ setup.py
@@ -2,16 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="ayudante",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         "numpy",
         "pandas",
         "matplotlib",
         "seaborn",
-        "scipy"
-        "scikit-learn "
+        "scipy",
+        "scikit-learn"
     ],
     author="Arman Ghaziaskari",
-    description="Librería personalizada para análisis EDA y regresiones",
+    description="Paquete para EDA, regresiones y evaluación automática de modelos.",
+    url="https://github.com/armanghazi/ayudante",
 )
